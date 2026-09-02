5459e34d7541ce2358a53cd41ee16ce5b632d7ce
Guilherme <guilhermesantos.srs@gmail.com>
2026-09-01 22:01:47 -0300

    Criar Endpoints para Visualizar e atualizar Notifications.

---
+++ src/main/java/com/ead/notification/dtos/NotificationDto.java
@@ -0,0 +1,12 @@
+package com.ead.notification.dtos;
+
+import com.ead.notification.enums.NotificationStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class NotificationDto {
+
+    @NotNull
+    private NotificationStatus notificationStatus;
+}
--- src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
+++ src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
@@ -1,9 +1,21 @@
 package com.ead.notification.services.impl;
 
+import com.ead.notification.enums.NotificationStatus;
 import com.ead.notification.models.NotificationModel;
 import com.ead.notification.repositories.NotificationRepository;
 import com.ead.notification.services.NotificationService;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
@@ -18,4 +30,16 @@ public class NotificationServiceImpl implements NotificationService {
     public NotificationModel saveNotification(NotificationModel notificationModel) {
         return notificationRepository.save(notificationModel);
     }
+
+    @Override
+    public Page<NotificationModel> findAllNotificationByUser(UUID userId, Pageable pageable) {
+        return notificationRepository.findAllByUserIdAndNotificationStatus(userId, NotificationStatus.CREATED, pageable);
+    }
+
+    @Override
+    public Optional<NotificationModel> findByNotificationIdAndUserId(UUID notificationId, UUID userId) {
+        return notificationRepository.findByNotificationIdAndUserId(notificationId, userId);
+    }
+
+
 }
